115916de5a3fdbdcdca09015281dcee996cbab4f
chenchuanwen <291966285@qq.com>
2019-07-05 09:41:29 +0800

    初始化

---
+++ PPTWeb/src/views/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class = "box">
+       <div class ="show" @click="elementClick">element</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data(){
+          return {
+
+          }
+        },
+        mounted(){
+
+        },
+        methods:{
+          elementClick(){
+            this.$message('这是一条消息提示');
+          },
+        }
+
+    }
+</script>
+
+<style lang="scss">
+.box{
+  width: 200px;
+  height: 200px;
+  border:1px solid black;
+  .show{
+    width: 100px;
+    height:100px;
+    border:1px solid red;
+  }
+}
+</style>
